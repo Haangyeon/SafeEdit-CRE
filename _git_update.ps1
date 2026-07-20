03c4aee08e57901f6570a99697e59f9aa2b38531
xiaangran0404 <xiaangran@163.com>
2026-07-21 00:00:21 +0800

    chore: remove accidentally committed temp script

--- _git_update.ps1
+++
@@ -1,6 +0,0 @@
-Set-Location "e:\Project\gene\SafeEdit-CRE"
-git add -A
-git config user.email "xiaangran@163.com"
-git config user.name "xiaangran0404"
-git commit -m "Final text consistency: version v1.0.0, locked benchmark wording, grammar fix"
-git push
